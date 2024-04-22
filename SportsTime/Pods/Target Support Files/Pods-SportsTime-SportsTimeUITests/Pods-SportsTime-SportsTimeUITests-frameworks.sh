9df257c9c0381ca448fc4d049eb83228d16b82b3
Raneem Ashraf <56923695+raneemashraf@users.noreply.github.com>
2024-04-22 22:29:26 +0200

    feat: Splash Screen Done

--- SportsTime/Pods/Target Support Files/Pods-SportsTime-SportsTimeUITests/Pods-SportsTime-SportsTimeUITests-frameworks.sh
+++ SportsTime/Pods/Target Support Files/Pods-SportsTime-SportsTimeUITests/Pods-SportsTime-SportsTimeUITests-frameworks.sh
@@ -181,6 +181,7 @@ if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/SDWebImage/SDWebImage.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SplashScreen/SplashScreen.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SwiftyOnboard/SwiftyOnboard.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/lottie-ios/Lottie.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
@@ -188,6 +189,7 @@ if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/SDWebImage/SDWebImage.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SplashScreen/SplashScreen.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SwiftyOnboard/SwiftyOnboard.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/lottie-ios/Lottie.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
---
+++ SportsTime/SportsTime/Splash/SplashViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  SportsTime
+//
+//  Created by Abdullah Essam on 22/04/2024.
+//
+
+import UIKit
+import Lottie
+
+class SplashViewController : UIViewController {
+    var animationView: LottieAnimationView?
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        animationView = .init(name: "sportLottie.json")
+        animationView!.frame = view.bounds
+        animationView!.contentMode = .scaleAspectFit
+        //animationView!.loopMode = .loop
+        view.addSubview(animationView!)
+        animationView!.play()
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 4){
+            self.performSegue(withIdentifier: "goToHome", sender: nil)
+        }
+    }
+}
+
