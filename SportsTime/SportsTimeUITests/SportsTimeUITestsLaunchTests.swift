c38e62dfbbdc00c7227fd105f61e3f51e23cda1a
AbdullahM98 <58375452+AbdullahM98@users.noreply.github.com>
2024-04-22 14:11:56 +0200

    init commit

---
+++ SportsTime/SportsTimeUITests/SportsTimeUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  SportsTimeUITestsLaunchTests.swift
+//  SportsTimeUITests
+//
+//  Created by Abdullah Essam on 22/04/2024.
+//
+
+import XCTest
+
+final class SportsTimeUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
