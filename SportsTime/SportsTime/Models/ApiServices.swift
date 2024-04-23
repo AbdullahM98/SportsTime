de37d90d1b268d807d7efafb8ed50a706410ff14
AbdullahM98 <58375452+AbdullahM98@users.noreply.github.com>
2024-04-23 20:50:03 +0200

    fix : Api Services created

--- SportsTime/SportsTime/Models/ApiServices.swift
+++ SportsTime/SportsTime/Models/ApiServices.swift
@@ -15,57 +15,7 @@ class ApiServices  {
         
     }
     
-    
-//    func fetchLeagues(completion: @escaping(Result<LeagueResponse,Error>) -> Void) {
-//
-//        let url = URL(string: "https://apiv2.allsportsapi.com/football/?met=Leagues&APIkey=2154818a4cbfc9dce69fab6771923c29e937839acc91aee84f9fa924bbbd4d6c")
-//        AF.request(url!,parameters:nil).validate().response{
-//            respon in
-//            switch respon.result{
-//            case .success(let data):
-//                do{
-//                    let jsonData = try JSONDecoder().decode(LeagueResponse.self, from: data!)
-//                    completion(.success(jsonData))
-//                    print(jsonData.result)
-//
-//                } catch {
-//                    print(error.localizedDescription)
-//                    completion(.failure(error))
-//
-//                }
-//            case .failure(let error):
-//                print(error.localizedDescription)
-//                completion(.failure(error))
-//
-//            }
-//
-//        }
-//    }
-//
-    func fetchLeagues(completion: @escaping(Result<LeagueResponse,Error>) -> Void) {
-        let url = URL(string: "https://apiv2.allsportsapi.com/football/?met=Leagues&APIkey=2154818a4cbfc9dce69fab6771923c29e937839acc91aee84f9fa924bbbd4d6c")
-        AF.request(url!, parameters: nil).validate().response { response in
-            switch response.result {
-            case .success(let data):
-                do {
-                    let jsonData = try JSONDecoder().decode(LeagueResponse.self, from: data!)
-                    completion(.success(jsonData))
-                    // Correctly print each League object's properties
-                    if let leagues = jsonData.result {
-//                        for league in leagues {
-//                            print("League Key: \(league.leagueKey ?? 0), League Name: \(league.leagueName ?? ""), Country Key: \(league.countryKey ?? 0), Country Name: \(league.countryName ?? ""), League Image: \(league.leagueImg ?? ""), Country Image: \(league.countryImg ?? "")")
-//                        }
-                    }
-                } catch {
-                    print("Error decoding JSON: \(error.localizedDescription)")
-                    completion(.failure(error))
-                }
-            case .failure(let error):
-                print("Request failed: \(error.localizedDescription)")
-                completion(.failure(error))
-            }
-        }
-    }
+
     
     func fetchLeaguesResult(sport: String, compilitionHandler: @escaping (LeagueResponse?) -> Void) {
         let url = URL(string: "https://apiv2.allsportsapi.com/football/?met=Leagues&APIkey=2154818a4cbfc9dce69fab6771923c29e937839acc91aee84f9fa924bbbd4d6c")
